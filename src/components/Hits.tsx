6ee2f56e2eec6a3a816ceb662c4fb8f0edf20929
da-in <talown@naver.com>
2022-12-27 23:21:02 +0900

    :lipstick: Update Badge
    Update Badge

--- src/components/Hits.tsx
+++ src/components/Hits.tsx
@@ -7,7 +7,7 @@ function Hits() {
   return (
     <a href="https://hits.seeyoufarm.com" className={badge}>
       <img
-        src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Froundshoulder.github.io%2Fblack_rabbit%2F&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"
+        src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Froundshoulder.github.io%2Fblack_rabbit&count_bg=%23AA1FFF&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=%EB%96%A1+%ED%83%80%EA%B0%84+%EC%82%AC%EB%9E%8C&edge_flat=false"
         alt="badge"
       />
     </a>
