e7bb8f81c7b80c10504165da5d117e8ec6998749
da-in <talown@naver.com>
2022-12-29 18:01:33 +0900

    :bug: Fix Adfit Container Position
    Fix Adfit Container Position

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -5,6 +5,7 @@ import Adfit from './Adfit';
 function Container({ children }: { children: ReactNode }) {
   const container = css`
     display: flex;
+    flex-direction: column;
     justify-content: center;
     width: 100%;
   `;
