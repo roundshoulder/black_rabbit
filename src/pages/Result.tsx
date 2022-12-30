3195ef6ef96f61ca2685b4466e3d9e59a49b6298
da-in <talown@naver.com>
2022-12-30 22:13:57 +0900

    :lipstick: Update Result Page
    Update Result Page

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -2,8 +2,11 @@ import { useNavigate } from 'react-router-dom';
 import { getResult } from '../components/results';
 import result_hidden from '../static/images/result_hidden.svg';
 import result_sun from '../static/images/result_sun.svg';
-import hand from '../static/images/hand.png';
+import result_good from '../static/images/result_good.svg';
+import result_bad from '../static/images/result_bad.svg';
+import result_stamp from '../static/images/result_stamp.svg';
 import { css } from '@emotion/css';
+import BottomButton from '../components/BottomButton';
 
 function Result() {
   const navigate = useNavigate();
@@ -26,6 +29,7 @@ function Result() {
 
   const sunContainer = css`
     width: 100%;
+    height: 170px;
     position: relative;
     display: flex;
     justify-content: center;
@@ -39,17 +43,21 @@ function Result() {
     justify-content: space-around;
   `;
 
-  const text = css`
+  const infoContainer = css`
+    background-color: white;
     padding: 10px;
+    border: solid 2px black;
+    border-radius: 10px;
+  `;
+
+  const text = css`
     font-size: 12px;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
       'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
       'Helvetica Neue', sans-serif;
     white-space: pre-line;
     text-align: center;
-    margin-bottom: 10px;
-    font-weight: normal;
-    line-height: 24px;
+    line-height: 22px;
   `;
 
   return (
@@ -57,7 +65,7 @@ function Result() {
       {wish && (
         <div className={container}>
           <p>2023 행운의 떡</p>
-          <span style={{ fontSize: '24px' }}>{result.name}</span>
+          <p style={{ fontSize: '24px' }}>{result.name}</p>
           <div className={sunContainer}>
             <div
               style={{
@@ -69,18 +77,37 @@ function Result() {
             >
               <img src={result_sun} alt="sun" />
             </div>
-            <img src={result.img} alt={result.name} style={{ zIndex: '10' }} />
+            <img
+              src={result.img}
+              alt={result.name}
+              style={{ zIndex: '10', width: '30%' }}
+            />
           </div>
-          <span className={text}>{result.info}</span>
-          <a
-            href="https://instagram.com/magic_heukki?igshid=MWI4MTIyMDE="
-            className={instagram}
-          >
-            <img src={hand} alt="다른 떡 구경하기" style={{ width: '40px' }} />{' '}
-            다른 떡 구경하기
-          </a>
+          <div className={infoContainer}>
+            <p className={text}>{result.info}</p>
+            <div
+              style={{
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'flex-end',
+              }}
+            >
+              <img src={result_stamp} alt="stamp" />
+            </div>
+          </div>
         </div>
       )}
+      <img src={result_good} alt="뽀뽀쪽" />
+      <img src={result_bad} alt="빌런 토끼" />
+
+      <a href="https://instagram.com/magic_heukki?igshid=MWI4MTIyMDE=">
+        <BottomButton
+          text="다른 떡 구경하기"
+          onclick={() => {}}
+          hasClick={true}
+        />
+      </a>
+      <p style={{ color: 'red' }}>* 경고 : 스크롤을 내리지 마시오.</p>
       <div style={{ position: 'relative', width: '100%', height: '400px' }}>
         <img
           src={result_hidden}
