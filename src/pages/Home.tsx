ac404dd7d7c542177f0c4acc6613ffc89a23cbb9
da-in <talown@naver.com>
2023-01-04 20:26:18 +0900

    :sparkles: Update 10000 Event
    Update 10000 Event

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import Hits from '../components/Hits';
 import home from '../static/images/home.svg';
 import counter_bg from '../static/images/counter_bg.svg';
+import event from '../static/images/event.svg';
 
 function Home() {
   const navigate = useNavigate();
@@ -26,6 +27,10 @@ function Home() {
         text="새해목표 쓰고 마법떡 받기"
         onclick={() => navigate('/create')}
         hasClick={true}
+      />
+      <img
+        src={event}
+        alt="10,000번째에 가장 가까운 사람 찾아요 인스타에 흑끼태그해서 올리면....!(비밀) @magic_heukki"
       />
       <div className={counterContainer}>
         <div style={{ position: 'absolute', top: '0px', right: '-60px' }}>
