d7e4ad9efddadfa486d837e900198aa4cda1b094
da-in <talown@naver.com>
2023-01-09 21:41:49 +0900

    :bento: Update Events
    Update Events

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -28,10 +28,12 @@ function Home() {
         onclick={() => navigate('/create')}
         hasClick={true}
       />
-      <img
-        src={event}
-        alt="10,000번째에 가장 가까운 사람 찾아요 인스타에 흑끼태그해서 올리면....!(비밀) @magic_heukki"
-      />
+      <a href="https://www.instagram.com/magic_heukki/">
+        <img
+          src={event}
+          alt="Event! 흑끼가 스타벅스 커피쏜다 일단 눌러봐 @magic_heukki"
+        />
+      </a>
       <div className={counterContainer}>
         <div style={{ position: 'absolute', top: '0px', right: '-60px' }}>
           <img src={counter_bg} alt="떡 타간 사람 복받음" />
